f391e65c07e787b4857a1f009e4bc18ce634edfe
mub2332 <43363470+mub2332@users.noreply.github.com>
2019-01-05 20:30:06 +1100

    added burgerbuilder component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
-import './App.css';
+import './App.module.css';
 
 import Layout from './components/Layout/Layout';
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div>
         <Layout>
-          <p>Test</p>
+          <BurgerBuilder></BurgerBuilder>
         </Layout>
       </div>
     );
--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,13 +1,14 @@
 import React from 'react';
 
 import Aux from '../../hoc/AuxWrap';
+import classes from './Layout.module.css';
 
 const layout = (props) => (
     <Aux>
         <div>
             Toolbar, sidedrawer, backdrop
         </div>
-        <main>
+        <main className={classes.Content}>
             {props.children}
         </main>
     </Aux>
