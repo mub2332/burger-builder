8018ac3b375dca00d0e4ded87eec418811e557dd
mub2332 <43363470+mub2332@users.noreply.github.com>
2019-01-06 17:44:17 +1100

    rendered burger with ingredients

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 
 import Aux from '../../hoc/AuxWrap';
+import Burger from '../../components/Burger/Burger';
 
 class BurgerBuilder extends Component {
 
     render () {
         return (
             <Aux>
-                <div>Burger</div>
+                <Burger></Burger>
                 <div>Build Controls</div>
             </Aux>
         );
