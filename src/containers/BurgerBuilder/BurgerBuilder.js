775ab74c027c4b4a7d16e4f50e2cfe4c2a3cff2d
mub2332 <43363470+mub2332@users.noreply.github.com>
2019-01-10 09:00:26 +1100

    made modal appear on clicking order button

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -23,7 +23,8 @@ class BurgerBuilder extends Component {
             meat: 0
         },
         totalPrice: 4,
-        purchasable: false
+        purchasable: false,
+        purchasing: false
     }
 
     updatePurchaseState(ingredients) {
@@ -89,6 +90,12 @@ class BurgerBuilder extends Component {
         
     }
 
+    purchaseHandler = () => {
+        this.setState({
+            purchasing: true
+        });
+    }
+
     render () {
         const disabledInfo = {
             ...this.state.ingredients
@@ -100,7 +107,7 @@ class BurgerBuilder extends Component {
 
         return (
             <Aux>
-                <Modal>
+                <Modal show={this.state.purchasing}>
                     <OrderSummary
                         ingredients={this.state.ingredients}>
                     </OrderSummary>
@@ -111,7 +118,8 @@ class BurgerBuilder extends Component {
                     ingredientRemoved={this.removeIngredientHandler}
                     disabled={disabledInfo}
                     price={this.state.totalPrice}
-                    purchasable={this.state.purchasable} >
+                    purchasable={this.state.purchasable}
+                    ordered={this.purchaseHandler} >
                 </BuildControls>
             </Aux>
         );
