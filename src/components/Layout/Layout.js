6f4a2305295226f94d1935067394496ee4cbd9fc
mub2332 <43363470+mub2332@users.noreply.github.com>
2019-01-11 18:05:59 +1100

    added toolbar

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,13 +1,12 @@
 import React from 'react';
 
-import Aux from '../../hoc/AuxWrap';
 import classes from './Layout.module.css';
+import Aux from '../../hoc/AuxWrap';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
 
 const layout = (props) => (
     <Aux>
-        <div>
-            Toolbar, sidedrawer, backdrop
-        </div>
+        <Toolbar></Toolbar>
         <main className={classes.Content}>
             {props.children}
         </main>
