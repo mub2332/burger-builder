e0f829863efc04e5ff3bc46fea6304e9ed530e6e
mub2332 <43363470+mub2332@users.noreply.github.com>
2019-01-12 15:08:42 +1100

    added ability to make sidedrawer disappear by clicking backdrop

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,18 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import classes from './Layout.module.css';
 import Aux from '../../hoc/AuxWrap';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import Sidedrawer from '../Navigation/Sidedrawer/Sidedrawer';
 
-const layout = (props) => (
-    <Aux>
-        <Toolbar></Toolbar>
-        <Sidedrawer></Sidedrawer>
-        <main className={classes.Content}>
-            {props.children}
-        </main>
-    </Aux>
-);
+class Layout extends Component {
 
-export default layout;
+    state = {
+        showSidedrawer: true
+    }
+
+    sidedrawerCloseHandler = () => {
+        this.setState({
+            showSidedrawer: false
+        });
+    }
+
+    render() {
+        return (
+            <Aux>
+                <Toolbar></Toolbar>
+                <Sidedrawer 
+                    open={this.state.showSidedrawer} 
+                    closed={this.sidedrawerCloseHandler}></Sidedrawer>
+                <main className={classes.Content}>
+                    {this.props.children}
+                </main>
+            </Aux>
+        )
+    }
+}
+
+export default Layout;
--- src/components/Navigation/Sidedrawer/Sidedrawer.js
+++ src/components/Navigation/Sidedrawer/Sidedrawer.js
@@ -3,16 +3,29 @@ import React from 'react';
 import classes from './Sidedrawer.module.css';
 import Logo from '../../Logo/Logo';
 import NavItems from '../NavItems/NavItems';
+import Backdrop from '../../UI/Backdrop/Backdrop';
+import Aux from '../../../hoc/AuxWrap';
 
-const sidedrawer = (props) => (
-    <div className={classes.Sidedrawer}>
-        <div class={classes.Logo}>
-            <Logo></Logo>
-        </div>
-        <nav>
-            <NavItems></NavItems>
-        </nav>
-    </div>
-);
+const sidedrawer = (props) => {
+    let attachedClasses = [classes.Sidedrawer, classes.Close];
+
+    if (props.open) {
+        attachedClasses = [classes.Sidedrawer, classes.Open];
+    }
+
+    return (
+        <Aux>
+            <Backdrop show={props.open} clicked={props.closed}></Backdrop>
+            <div className={attachedClasses.join(' ')}>
+                <div class={classes.Logo}>
+                    <Logo></Logo>
+                </div>
+                <nav>
+                    <NavItems></NavItems>
+                </nav>
+            </div>
+        </Aux>
+    );
+};
 
 export default sidedrawer;
