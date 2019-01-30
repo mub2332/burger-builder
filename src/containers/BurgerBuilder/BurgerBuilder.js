c3c4ce9fa0140617edacf382b908009641478c1a
mub2332 <43363470+mub2332@users.noreply.github.com>
2019-01-31 02:45:48 +1100

    Added firebase integration, enabled order POST to server

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,145 +1,163 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import Aux from '../../hoc/AuxWrap/AuxWrap';
-import Burger from '../../components/Burger/Burger';
-import BuildControls from '../../components/Burger/BuildControls/BuildControls';
-import Modal from '../../components/UI/Modal/Modal';
-import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+import Aux from "../../hoc/AuxWrap/AuxWrap";
+import Burger from "../../components/Burger/Burger";
+import BuildControls from "../../components/Burger/BuildControls/BuildControls";
+import Modal from "../../components/UI/Modal/Modal";
+import OrderSummary from "../../components/Burger/OrderSummary/OrderSummary";
+import axios from "../../axios-orders";
 
 const INGREDIENT_PRICES = {
-    salad: 0.5,
-    cheese: 0.4,
-    meat: 1.3,
-    bacon: 0.7
+  salad: 0.5,
+  cheese: 0.4,
+  meat: 1.3,
+  bacon: 0.7
 };
 
 class BurgerBuilder extends Component {
+  state = {
+    ingredients: {
+      salad: 0,
+      bacon: 0,
+      cheese: 0,
+      meat: 0
+    },
+    totalPrice: 4,
+    purchasable: false,
+    purchasing: false
+  };
 
-    state = {
-        ingredients: {
-            salad: 0,
-            bacon: 0,
-            cheese: 0,
-            meat: 0
+  updatePurchaseState(ingredients) {
+    const sum = Object.keys(ingredients)
+      .map(key => {
+        return ingredients[key];
+      })
+      .reduce((sum, el) => {
+        return sum + el;
+      }, 0);
+
+    this.setState({
+      purchasable: sum > 0
+    });
+  }
+
+  addIngredientHandler = type => {
+    const oldCount = this.state.ingredients[type];
+    const newCount = oldCount + 1;
+
+    const updatedIngredients = {
+      ...this.state.ingredients
+    };
+    updatedIngredients[type] = newCount;
+
+    const priceAddition = INGREDIENT_PRICES[type];
+    const oldPrice = this.state.totalPrice;
+    const newPrice = oldPrice + priceAddition;
+
+    this.setState({
+      ingredients: updatedIngredients,
+      totalPrice: newPrice
+    });
+
+    this.updatePurchaseState(updatedIngredients);
+  };
+
+  removeIngredientHandler = type => {
+    const oldCount = this.state.ingredients[type];
+
+    if (oldCount <= 0) {
+      return;
+    }
+
+    const newCount = oldCount - 1;
+
+    const updatedIngredients = {
+      ...this.state.ingredients
+    };
+    updatedIngredients[type] = newCount;
+
+    const priceDeduction = INGREDIENT_PRICES[type];
+    const oldPrice = this.state.totalPrice;
+    const newPrice = oldPrice - priceDeduction;
+
+    this.setState({
+      ingredients: updatedIngredients,
+      totalPrice: newPrice
+    });
+
+    this.updatePurchaseState(updatedIngredients);
+  };
+
+  purchaseHandler = () => {
+    this.setState({
+      purchasing: true
+    });
+  };
+
+  purchaseCancelHandler = () => {
+    this.setState({
+      purchasing: false
+    });
+  };
+
+  purchaseContinueHandleer = () => {
+    const order = {
+      ingredients: this.state.ingredients,
+      price: this.state.totalPrice,
+      customer: {
+        name: "Mubtasim Mahmud",
+        address: {
+          street: "hello",
+          zipcode: "6969",
+          country: "world"
         },
-        totalPrice: 4,
-        purchasable: false,
-        purchasing: false
+        email: "hz@wteva.org"
+      },
+      delivery: "express"
+    };
+
+    axios
+      .post("/orders.json", order)
+      .then(response => {
+        console.log(response);
+      })
+      .catch(error => console.log(error));
+  };
+
+  render() {
+    const disabledInfo = {
+      ...this.state.ingredients
+    };
+
+    for (let key in disabledInfo) {
+      disabledInfo[key] = disabledInfo[key] <= 0;
     }
 
-    updatePurchaseState(ingredients) {
-        const sum = Object.keys(ingredients)
-            .map(key => {
-                return ingredients[key];
-            })
-            .reduce((sum, el) => {
-                return sum + el;
-            }, 0);
-
-        this.setState({
-            purchasable: sum > 0
-        });
-    }
-
-    addIngredientHandler = (type) => {
-        const oldCount = this.state.ingredients[type];
-        const newCount = oldCount + 1;
-
-        const updatedIngredients = {
-            ...this.state.ingredients
-        };
-        updatedIngredients[type] = newCount;
-
-        const priceAddition = INGREDIENT_PRICES[type];
-        const oldPrice = this.state.totalPrice;
-        const newPrice = oldPrice + priceAddition;
-
-        this.setState({
-            ingredients: updatedIngredients,
-            totalPrice: newPrice
-        });
-
-        this.updatePurchaseState(updatedIngredients);
-
-    }
-
-    removeIngredientHandler = (type) => {
-        const oldCount = this.state.ingredients[type];
-
-        if (oldCount <= 0) {
-            return;
-        }
-
-        const newCount = oldCount - 1;
-
-        const updatedIngredients = {
-            ...this.state.ingredients
-        };
-        updatedIngredients[type] = newCount;
-
-        const priceDeduction = INGREDIENT_PRICES[type];
-        const oldPrice = this.state.totalPrice;
-        const newPrice = oldPrice - priceDeduction;
-
-        this.setState({
-            ingredients: updatedIngredients,
-            totalPrice: newPrice
-        });
-
-        this.updatePurchaseState(updatedIngredients);
-        
-    }
-
-    purchaseHandler = () => {
-        this.setState({
-            purchasing: true
-        });
-    }
-
-    purchaseCancelHandler = () => {
-        this.setState({
-            purchasing: false
-        });
-    }
-
-    purchaseContinueHandleer = () => {
-        alert('continue');
-    }
-
-    render () {
-        const disabledInfo = {
-            ...this.state.ingredients
-        };
-
-        for (let key in disabledInfo) {
-            disabledInfo[key] = disabledInfo[key] <= 0;
-        }
-
-        return (
-            <Aux>
-                <Modal 
-                    show={this.state.purchasing}
-                    modalClosed={this.purchaseCancelHandler} >
-                    <OrderSummary
-                        ingredients={this.state.ingredients}
-                        purchaseCanceled={this.purchaseCancelHandler}
-                        purchaseContinued={this.purchaseContinueHandleer}
-                        price={this.state.totalPrice} >
-                    </OrderSummary>
-                </Modal>
-                <Burger ingredients={this.state.ingredients}></Burger>
-                <BuildControls
-                    ingredientAdded={this.addIngredientHandler}
-                    ingredientRemoved={this.removeIngredientHandler}
-                    disabled={disabledInfo}
-                    price={this.state.totalPrice}
-                    purchasable={this.state.purchasable}
-                    ordered={this.purchaseHandler} >
-                </BuildControls>
-            </Aux>
-        );
-    }
-
+    return (
+      <Aux>
+        <Modal
+          show={this.state.purchasing}
+          modalClosed={this.purchaseCancelHandler}
+        >
+          <OrderSummary
+            ingredients={this.state.ingredients}
+            purchaseCanceled={this.purchaseCancelHandler}
+            purchaseContinued={this.purchaseContinueHandleer}
+            price={this.state.totalPrice}
+          />
+        </Modal>
+        <Burger ingredients={this.state.ingredients} />
+        <BuildControls
+          ingredientAdded={this.addIngredientHandler}
+          ingredientRemoved={this.removeIngredientHandler}
+          disabled={disabledInfo}
+          price={this.state.totalPrice}
+          purchasable={this.state.purchasable}
+          ordered={this.purchaseHandler}
+        />
+      </Aux>
+    );
+  }
 }
 
 export default BurgerBuilder;
--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,45 +1,41 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import classes from './Layout.module.css';
-import Aux from '../AuxWrap/AuxWrap';
-import Toolbar from '../../components/Navigation/Toolbar/Toolbar';
-import Sidedrawer from '../../components/Navigation/Sidedrawer/Sidedrawer';
+import classes from "./Layout.module.css";
+import Aux from "../AuxWrap/AuxWrap";
+import Toolbar from "../../components/Navigation/Toolbar/Toolbar";
+import Sidedrawer from "../../components/Navigation/Sidedrawer/Sidedrawer";
 
 class Layout extends Component {
+  state = {
+    showSidedrawer: false
+  };
 
-    state = {
-        showSidedrawer: false
-    }
+  sidedrawerCloseHandler = () => {
+    this.setState({
+      showSidedrawer: false
+    });
+  };
 
-    sidedrawerCloseHandler = () => {
-        this.setState({
-            showSidedrawer: false
-        });
-    }
+  sidedrawerToggleHandler = () => {
+    this.setState(prevState => {
+      return {
+        showSidedrawer: !prevState.showSidedrawer
+      };
+    });
+  };
 
-    sidedrawerToggleHandler = () => {
-        this.setState(
-            (prevState) => {
-                return {
-                    showSidedrawer: !prevState.showSidedrawer
-                };
-            }
-        )
-    }
-
-    render() {
-        return (
-            <Aux>
-                <Toolbar drawerToggleClicked={this.sidedrawerToggleHandler}></Toolbar>
-                <Sidedrawer 
-                    open={this.state.showSidedrawer} 
-                    closed={this.sidedrawerCloseHandler}></Sidedrawer>
-                <main className={classes.Content}>
-                    {this.props.children}
-                </main>
-            </Aux>
-        )
-    }
+  render() {
+    return (
+      <Aux>
+        <Toolbar drawerToggleClicked={this.sidedrawerToggleHandler} />
+        <Sidedrawer
+          open={this.state.showSidedrawer}
+          closed={this.sidedrawerCloseHandler}
+        />
+        <main className={classes.Content}>{this.props.children}</main>
+      </Aux>
+    );
+  }
 }
 
 export default Layout;
